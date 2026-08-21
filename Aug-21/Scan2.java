854f248c2e8f8d9d35cde8eb34737cc4ae256953
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-08-21 11:42:16 +0530

    code

---
+++ Aug-21/Scan2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Scan2 {
+	public static void main(String[] args) {
+		int a, b, c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 Numbers  ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = a + b;
+		System.out.println("Sum is  " +c);
+		c = a - b;
+		System.out.println("Sub is  " +c);
+		c = a * b;
+		System.out.println("Mult is  " +c);
+	}
+}
