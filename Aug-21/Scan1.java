6d56022748c315de1a93681c1ad8453a68c3d21e
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-08-21 11:24:14 +0530

    code

---
+++ Aug-21/Scan1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Scan1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name;
+		System.out.println("Enter Name  ");
+		name = sc.nextLine();
+		System.out.println("Name is " +name);
+	}
+}
