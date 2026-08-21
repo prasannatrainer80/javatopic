186903181cfcb1070f7e53e9aa184674c3bbd274
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-08-21 11:15:14 +0530

    code

---
+++ Aug-21/Example4.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Example4 {
+	public static void main(String[] args) {
+		Set names = new LinkedHashSet();
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		System.out.println("Names Are  ");
+		for (Object object : names) {
+			System.out.println(object);
+		}
+		
+	}
+}
