2fe2583decf0649b71fd98cc3001b4acfb7baad6
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-08-21 11:08:05 +0530

    code

---
+++ Aug-21/Example3.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example3 {
+	public static void main(String[] args) {
+		Set names = new HashSet();
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		names.add("Sanjana");
+		names.add("Sagar");
+		names.add("Nishitha");
+		names.add("Hemanth");
+		names.add("Anil");
+		names.add("Srujana");
+		System.out.println("Names Are  ");
+		for (Object object : names) {
+			System.out.println(object);
+		}
+		
+	}
+}
