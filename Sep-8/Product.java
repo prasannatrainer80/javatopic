d17bdf01229fe20d9af195efff1370460f42259e
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-09-08 14:56:14 +0530

    code

---
+++ Sep-8/Product.java
@@ -0,0 +1,22 @@
+
+public class Product {
+
+		int id;  
+	    String name;  
+	    double price;
+	    
+	    public Product() {
+
+	    }
+
+		public Product(int id, String name, double price) {  
+	        this.id = id;  
+	        this.name = name;  
+	        this.price = price;  
+	    }
+	    
+	    @Override
+	    public String toString() {
+	    	return "Id " +id+ " Name " +name+ " Price " +price;
+	    }
+}
