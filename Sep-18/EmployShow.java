1c9f2e5c9cc554eac28264aa41ed2dc45a0512d1
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2026-09-18 10:07:36 +0530

    code

---
+++ Sep-18/EmployShow.java
@@ -0,0 +1,34 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployShow {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = 
+				DriverManager.getConnection("jdbc:mysql://localhost:3306/crt","root","root");
+			String cmd = "select * from Employ";
+			PreparedStatement pst = connection.prepareStatement(cmd);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				System.out.println("Employ No  " +rs.getInt("empno"));
+				System.out.println("Employ Name  " +rs.getString("name"));
+				System.out.println("Gender  " +rs.getString("gender"));
+				System.out.println("Department  " +rs.getString("dept"));
+				System.out.println("Designation  " +rs.getString("desig"));
+				System.out.println("Basic  " +rs.getString("basic"));
+				System.out.println("--------------------------------");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
